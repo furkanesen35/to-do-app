b4a92db4ada7a80331aa50f388cde2f1c3d38fe3
furkan <furkanesen35@gmail.com>
2022-06-21 14:25:11 +0200

    buttons and inputs done storage side left

--- functions.js
+++ functions.js
@@ -1,3 +1,37 @@
 const btn = document.getElementById("todo-button");
 const todoInput = document.getElementById("todo-input");
-console.log(btn,todoInput);
+const todoUl = document.getElementById("todo-ul");
+
+btn.addEventListener("click", (e)=> {
+ if(!todoInput.value){
+  alert("Please enter an assignment");
+  return;
+ } 
+ else {
+  todoUl.innerHTML += 
+  `<li>
+   <i class="fa fa-check"></i>
+   <p>${todoInput.value}</p>
+   <i class="fa fa-trash"></i>
+  </li>`;
+ }
+});
+
+todoInput.addEventListener("keydown", (e) => {
+ if(e.key === "Enter"){
+  btn.click();
+ }
+})
+
+todoUl.addEventListener("click", (e)=> {
+ if(e.target.classList.contains("fa-trash")){
+  e.target.parentElement.remove();
+ }
+ else if(e.target.classList.contains("fa-check")){
+  if(e.target.parentElement.classList.contains("checked")){
+   e.target.parentElement.classList.remove("checked")
+  } else {
+   e.target.parentElement.classList.add("checked")
+  }
+ }
+})
